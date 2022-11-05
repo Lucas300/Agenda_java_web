ced8452189f72e88ad4e2bc4dc524031d80d1bd6
Lucas Daniel <ludaniel.sd@gmail.com>
2022-11-05 04:29:47 -0300

    Criando a conexão com o banco de dados, Criando a Classe DAO, baixando o diver de conexão no site do mysql e desconpactando e colocando na pasta lib do projeto e importando o DAO no Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO; // é necessario importar a classe DAO e criar um objeto; part 1
+
 
 /**
  * Servlet implementation class Controller
@@ -13,7 +15,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns={"/Controller" , "/main"}) // Adicionado , main é uma requisição que o servlet vai trabalhar
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
+       DAO dao = new DAO();    // objeto criado para a importação DAO part 2
+    
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,6 +33,10 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		/* TESTANDO CONEXÃO
+			dao.testeConexao(); //para fazer o teste de conexão part 3
+		*/
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,39 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** MODULO DE CONEXÃO **/
+	//PARAMENTROS DE CONEXÃO
+	
+	private String driver = "com.mysql.cj.jdbc.Driver"; 
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC"; //ip , nome do banco de dados , horario de referencia
+	private String user = "root";
+	private String password = "root";
+	
+	//MÉTODOS DE CONEXÃO
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.print(e);
+			return null;
+		}
+	}
+	
+	/* teste de conexão  
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	*/
 }
