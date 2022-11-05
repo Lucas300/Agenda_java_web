1f9dc1beec8bde5421780045db847ad91bceef11
Lucas Daniel <ludaniel.sd@gmail.com>
2022-11-05 05:12:08 -0300

    Configurando a camada controller e criando uma variavel para salvar o caminho da requisição

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,35 +8,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO; // é necessario importar a classe DAO e criar um objeto; part 1
 
-
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns={"/Controller" , "/main"}) // Adicionado , main é uma requisição que o servlet vai trabalhar
+@WebServlet(urlPatterns = { "/Controller", "/main" }) // Adicionado , main é uma requisição que o servlet vai trabalhar
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-       DAO dao = new DAO();    // objeto criado para a importação DAO part 2
-    
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	DAO dao = new DAO(); // objeto criado para a importação DAO part 2
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		/* TESTANDO CONEXÃO
-			dao.testeConexao(); //para fazer o teste de conexão part 3
-		*/
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath(); // a variavel vai armazenar o caminho da requição
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
+	}
+
+	// listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 }
---
+++ src/main/webapp/agenda.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Agenda de contatos</title>
+<link rel="icon" href="imagens/telefone.png">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Agenda de contatos</h1>
+	<a class="botao1" style="margin-left:150px;" href="">Novo contato</a>
+</body>
+</html>
